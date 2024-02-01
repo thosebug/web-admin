f5c6536b26856ae26aa3765f1e8e33ccde1925ef
ther <cuitao2000@126.com>
2024-02-01 15:38:31 +0800

    eslint修复

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -4,70 +4,76 @@
       :key="tableKey"
       v-loading="listLoading"
       :data="list"
+      border
       fit
       highlight-current-row
-      style="width: 100%;"
+      style="width: 100%"
     >
+      <el-table-column type="selection" width="50" />
       <el-table-column label="编号" prop="id" width="200px" align="center">
-        <template slot-scope="{row}">
+        <template slot-scope="{ row }">
           <span>{{ row.id }}</span>
         </template>
       </el-table-column>
       <el-table-column label="用户名" width="150px" align="center">
-        <template slot-scope="{row}">
+        <template slot-scope="{ row }">
           <span>{{ row.username }}</span>
         </template>
       </el-table-column>
       <el-table-column label="昵称" width="150px" align="center">
-        <template slot-scope="{row}">
+        <template slot-scope="{ row }">
           <span>{{ row.account }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="头像" width="110px" align="center">
-        <template slot-scope="{row}">
-          <el-image :src="row.avatar" :fit="`scale-down`"></el-image>
+      <el-table-column label="头像" width="60px" align="center">
+        <template slot-scope="{ row }">
+          <el-image
+            :src="row.avatar"
+            :fit="cover"
+            style="width: 40px; height: 40px"
+          />
         </template>
       </el-table-column>
-      <el-table-column label="简介" width="300px" align="center">
-        <template slot-scope="{row}">
-          <span v-if="row.profile!==null">{{ row.profile }}</span>
+      <el-table-column label="简介" width="400px" align="center">
+        <template slot-scope="{ row }">
+          <span v-if="row.profile !== null">{{ row.profile }}</span>
           <span v-else>用户很懒，什么都没有写！</span>
         </template>
       </el-table-column>
-      <el-table-column label="角色" width="80px">
-        <template slot-scope="{row}">
+      <el-table-column label="角色" width="65px">
+        <template slot-scope="{ row }">
           <el-tag :type="row.role | roleFilter">
             {{ row.role }}
           </el-tag>
         </template>
       </el-table-column>
       <el-table-column label="创建时间" width="180px">
-        <template slot-scope="{row}">
+        <template slot-scope="{ row }">
           <span>{{ row.createTime }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="操作" align="center" width="400px" class-name="small-padding fixed-width">
-        <template slot-scope="{row}">
-          <el-button type="primary" size="mini">
-            查看
-          </el-button>
-          <el-button type="success" size="mini">
-            编辑
-          </el-button>
-          <el-button v-if="row.role!=='ban'" size="mini">
-            禁用
-          </el-button>
-          <el-button v-else size="mini">
-            启用
-          </el-button>
-          <el-button type="danger" size="mini">
-            删除
-          </el-button>
+      <el-table-column
+        label="操作"
+        align="center"
+        width="400px"
+        class-name="small-padding fixed-width"
+      >
+        <template slot-scope="{ row }">
+          <el-button type="primary" size="mini"> 查看 </el-button>
+          <el-button type="success" size="mini"> 编辑 </el-button>
+          <el-button v-if="row.role !== 'ban'" size="mini"> 禁用 </el-button>
+          <el-button v-else size="mini"> 启用 </el-button>
+          <el-button type="danger" size="mini"> 删除 </el-button>
         </template>
       </el-table-column>
     </el-table>
-
-    <pagination v-show="total>0" :total="total" :page.sync="listQuery.current" :limit.sync="listQuery.size" @pagination="getUserList" />
+    <pagination
+      v-show="total > 0"
+      :total="total"
+      :page.sync="listQuery.current"
+      :limit.sync="listQuery.size"
+      @pagination="getUserList"
+    />
   </div>
 </template>
 
@@ -85,7 +91,7 @@ export default {
         ban: 'danger'
       }
       return roleMap[role]
-    },
+    }
   },
   data() {
     return {
@@ -108,7 +114,7 @@ export default {
   methods: {
     getUserList() {
       this.listLoading = true
-      this.$store.dispatch('user/getPage', this.listQuery).then((response) => {
+      this.$store.dispatch("user/getPage", this.listQuery).then((response) => {
         this.list = response.records
         this.total = parseInt(response.total)
         // Just to simulate the time of the request
@@ -121,6 +127,4 @@ export default {
 }
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
